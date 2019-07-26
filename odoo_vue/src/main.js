a2bc0aa4e30101aa47dad6c2ba3e232dd147f57c
yuan <18817382841@163.com>
2019-07-26 19:18:32 +0800

    修复一些BUG

--- odoo_vue/src/main.js
+++ odoo_vue/src/main.js
@@ -138,12 +138,12 @@ Vue.config.productionTip = false
 // Before you create app
 Vue.config.devtools = process.env.NODE_ENV === 'development'
 /* eslint-disable no-new */
-let app = new Vue({
+new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app-box')
 // After you create app
-window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app.constructor
+// window.__VUE_DEVTOOLS_GLOBAL_HOOK__.Vue = app.constructor
 // then had to add in ./store.js as well.
 Vue.config.devtools = process.env.NODE_ENV === 'development'
